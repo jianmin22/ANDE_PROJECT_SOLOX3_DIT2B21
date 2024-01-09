b438aff7ac47926325bf1b1119b99360e136d6b9
jianmin22 <jianmin.22@ichat.sp.edu.sg>
2024-01-09 23:22:55 +0800

    set up firebase

---
+++ app/src/main/java/com/example/solox3_dit2b21/BookDetails.java
@@ -0,0 +1,14 @@
+package com.example.solox3_dit2b21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
