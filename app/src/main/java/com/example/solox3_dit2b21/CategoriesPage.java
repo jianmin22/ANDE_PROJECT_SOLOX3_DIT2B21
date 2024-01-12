07a305fb537ab2090ce3ceed60cc9ea73b6fb9ba
roy0513 <royheng33@gmail.com>
2024-01-12 12:00:06 +0800

    category page still in progress

---
+++ app/src/main/java/com/example/solox3_dit2b21/CategoriesPage.java
@@ -0,0 +1,14 @@
+package com.example.solox3_dit2b21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoriesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories_page);
+    }
+}
