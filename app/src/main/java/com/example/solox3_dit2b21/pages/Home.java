2aa5ec6f994c07a7cf2fa2f78f7a9682800c8419
Jian Min Soh <115980621+jianmin22@users.noreply.github.com>
2024-01-23 15:53:47 +0800

    correct

--- app/src/main/java/com/example/solox3_dit2b21/pages/Home.java
+++ app/src/main/java/com/example/solox3_dit2b21/pages/Home.java
@@ -42,7 +42,7 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
 
             if (itemId == R.id.navigation_bookshelf) {
                 Intent intent = new Intent(Home.this, EditorSpace.class);
-                intent.putExtra("bookId", "book1");
+                intent.putExtra("bookId", "book2");
                 startActivity(intent);
                 // Navigate to Bookshelf activity
                 // Replace CategoryPage.class with the correct Activity class for Bookshelf
